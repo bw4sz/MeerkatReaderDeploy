0c95178988e15cc262a99d3ecbee867a2cf935d0
bw4sz <benweinstein2010@gmail.com>
2017-04-22 09:38:30 -0700

    works for both tlv and mp4 test cases

--- ExtractNegatives.py
+++ ExtractNegatives.py
@@ -10,6 +10,7 @@ class Video:
         self.cap = cv2.VideoCapture(path)
         self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))        
         if self.length == 0:
+            print("setting length manually")
             self.length = 10000
         
     def random_image(self):
@@ -39,13 +40,16 @@ class Video:
                 if fcount in frame_number:            
                     ret,frame=self.cap.read()
                     frame_to_write.append(frame)
+                    fcount = fcount +1                     
         else:
             for f in frame_number:
-                self.cap.set(1,frame_number[f]) #the proper way for videos that can read the codec.
+                self.cap.set(1,f) #the proper way for videos that can read the codec.
                 ret,frame=self.cap.read()                
                 frame_to_write.append(frame)
         
         #write the frames    
+        print(len(frame_to_write))
+        
         for fr in frame_to_write:
             filename="C:/Users/Ben/Dropbox/GoogleCloud/Negatives/" + str(counter) +".jpg"
             cv2.imwrite(filename, fr)    
